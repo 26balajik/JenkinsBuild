2b812d7633c7ae16e0d58441dcb198bb89509d02
26balajik <56104207+26balajik@users.noreply.github.com>
2022-11-10 12:30:07 +0530

    Add files via upload
    New Version added

---
+++ com/balajiInfo/TestJava.java
@@ -0,0 +1,28 @@
+package com.balajiInfo;
+
+public class TestJava {
+
+	public static void main(String[] args) {
+		System.out.println("Hai Muni Welcome to help to get Good job in good Comapny as permenent job for 10 Years which is near to Electronic City");
+		B a1=new B();
+		//a1.v=100;
+		System.out.println("====>"+a1.getData());
+		
+	}
+
+}
+
+class A {
+	
+	 int c=10;
+}
+
+class B extends A
+{
+       private int v=c+20;
+      
+      public int getData()
+      {
+    	  return v;
+      }
+}
