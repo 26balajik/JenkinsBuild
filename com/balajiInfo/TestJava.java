52d32d853701e2b052525177a87d5dfb84bd3bb4
26balajik <56104207+26balajik@users.noreply.github.com>
2022-11-11 10:33:40 +0530

    Modify 2

--- com/balajiInfo/TestJava.java
+++ com/balajiInfo/TestJava.java
@@ -3,7 +3,7 @@ package com.balajiInfo;
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$Hai Muni Welcome to help to get Good job in good Comapny as permenent job for 10 Years which is near to Electronic City");
+		System.out.println("!!!!!!!Hai Muni Welcome to help to get Good job in good Comapny as permenent job for 10 Years which is near to Electronic City");
 		B a1=new B();
 		//a1.v=100;
 		System.out.println("====>"+a1.getData());
